5cd40d63f1542e5ebae6c25bc97f413d83519681
Binguner <zhengzuoyu0414@gmail.com>
2019-05-13 22:43:16 +0800

    start chapter09

---
+++ chapter09/src/main/java/com/itheima/test/MybatisAssociationedTest.java
@@ -0,0 +1,46 @@
+package com.itheima.test;
+
+import com.itheima.po.Orders;
+import com.itheima.po.Person;
+import com.itheima.po.User;
+import com.itheima.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class MybatisAssociationedTest {
+
+    private final String personPreStr = "com.itheima.mapper.PersonMapper.";
+    private final String idCardPreStr = "com.itheima.mapper.IdCardMapper.";
+    private final String userPreStr = "com.itheima.mapper.UserMapper.";
+    private final String ordersPreStr = "com.itheima.mapper.OrdersMapper.";
+    @Test
+    public void findPersonByIdTest(){
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        Person person = sqlSession.selectOne(personPreStr + "findPersonById", 1);
+        System.out.println(person);
+        sqlSession.close();
+    }
+
+    @Test
+    public void findPersonByIdTest2(){
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        Person person = sqlSession.selectOne(personPreStr+"findPersonById2",1);
+        System.out.println(person);
+        sqlSession.close();
+    }
+
+    @Test
+    public void findUserTest(){
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        User user = (User) sqlSession.selectOne(userPreStr+"findUserWithOrders",1);
+        System.out.println(user);
+        sqlSession.close();
+    }
+
+    @Test
+    public void findOrdersTest(){
+        SqlSession sqlSession = MyBatisUtils.getSession();
+//        Orders orders = sqlSession.selectOne(ordersPreStr+".")
+    }
+}
+
