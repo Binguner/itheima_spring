674561ee421d0a8cd0dbfecd7511372d730a4d67
Binguner <zhengzuoyu0414@gmail.com>
2019-05-14 22:40:20 +0800

    start chapter11

---
+++ chapter11/src/main/java/com/itheima/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.itheima.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class FirstController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "这是我的第一个 Spring MVC 程序");
+        modelAndView.setViewName("/WEB-INF/jsp/first.jsp");
+        return modelAndView;
+    }
+}
